404a5f639d017e3ba02859371a2aa8c96d1b00a5
srlane <scott.lane@ciholas.com>
2019-10-31 17:30:32 -0500

    Made javascript work for semantic ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,9 @@
 //= require jquery
 //= require turbolinks
 //= require semantic-ui
+//= require_self
 //= require_tree .
+
+$(document).on('turbolinks:load', function () {
+  $('.ui.dropdown').dropdown();
+})
