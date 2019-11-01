eb0dbbd5d1c0b2694f85ce88c977ce405d12c700
srlane <scott.lane@ciholas.com>
2019-11-01 14:50:23 -0500

    Limited the number of messages to display and improved input box functionality.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,26 @@
 //= require_self
 //= require_tree .
 
+scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);      
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+        $('button').click();
+        e.target.value = "";
+    };
+  });
+}
+
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).parent().hide();
   });
+  submit_message();
+  scroll_bottom();  
 })
