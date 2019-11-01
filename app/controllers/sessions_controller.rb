0de15a65302a233b650aa2ff43dee6ac3bdc4cc3
srlane <scott.lane@ciholas.com>
2019-11-01 12:16:37 -0500

    Added session handling.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to user_path(user)
+      redirect_to root_path
     else
-      flash.now[:danger] = "There was something wrong with your login information"
+      flash.now[:error] = "There was something wrong with your login information"
       render 'new'
     end
   end
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have logged out"
-    redirect_to root_path
+    redirect_to login_path
   end
 
 end
